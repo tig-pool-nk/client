d89b433b020c72c1a152a3a0eecd20f8a6d2cd19
François Patron <francois.patron@kandy.eu>
2024-12-12 14:38:36 +0100

    English traduction

--- scripts/pool_tig_launch_master.sh
+++ scripts/pool_tig_launch_master.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#!/bin/bash
 
-# Variables globales
+# Global variables
 id_slave="@id@"
 login_discord="@login@"
 token_private="@tok@"
-nom_machine="@worker@"
+machine_name="@worker@"
 
-# Serveur TIG
+# TIG Server
 ip="@ip@"
 port="6666"
 add_tig=""
@@ -15,41 +14,31 @@ add_tig=""
 # WEB + port 
 url="@url@"
 
-
-# Chemin du TIG, chemin absolu
+# Absolute path of TIG
 path_tig='@@path@@'
 
-# Nom du fichier client
-client_file="/client_xnico_pool/client_tig_pool_v@v@"
-# Nom du fichier client
+# Name of the client file
+client_file="bin/client_tig_pool"
 
-
-# Chemins relatifs à vérifier
-path_env="$path_tig/mon_env"
+# Relative paths to check
+path_env="$path_tig/venv"
 worker_path="$path_tig/tig-monorepo/target/release/tig-worker"
 
-
-# Vérifier si l'environnement virtuel Python existe
+# Check if the Python virtual environment exists
 if [ ! -d "$path_env" ]; then
-    echo "L'environnement virtuel mon_env n'existe pas. Veuillez le créer avant de continuer."
-    echo "Pour le créer, exécutez :"
+    echo "The virtual environment mon_env does not exist. Please create it before proceeding."
+    echo "To create it, run:"
     echo "python3 -m venv $path_env"
     exit 1
 fi
 
-# Vérifier si le worker est présent
+# Check if the worker file exists
 if [ ! -f "$worker_path" ]; then
-    echo "Le fichier tig-worker n'existe pas à l'emplacement $worker_path. Veuillez le build avant de continuer."
-    echo "Pour le build, exécutez :"
+    echo "The tig-worker file does not exist at $worker_path. Please build it before proceeding."
+    echo "To build it, run:"
     echo "cd $path_tig/tig-monorepo && cargo build --release"
     exit 1
 fi
 
-
-
-# Si les vérifications sont passées, exécuter le client Python
+# If checks pass, execute the Python client
 ./"$client_file" --path_to_tig "$path_tig" --id_slave "$id_slave" --login_discord "$login_discord" --token_private "$token_private" --ip "$ip"  --port "$port" --add_tig "$add_tig" --url "$url"
-
-
-
-
