3f9dccc4b2340bfff8bbee64407fdb6c139e0300
xnico31 <54291310+xnico31@users.noreply.github.com>
2024-12-20 23:27:09 +0100

    maj instal

--- install.sh
+++ install.sh
@@ -19,36 +19,35 @@ else
     mode=$8
     if [ "$mode" = "testnet" ]; then
         branch="test"
+
     elif [ "$mode" = "mainnet" ]; then
         branch="main"
+        
     else
         echo "Invalid mode: $mode"
         exit 1
     fi
 fi
 
-rm -rf tig_pool_$branch
-mkdir tig_pool_$branch
-cd tig_pool_$branch
+# Remove existing directory and recreate
+rm -rf "tig_pool_$branch"
+mkdir "tig_pool_$branch"
+cd "tig_pool_$branch" || exit 1
 
-
-slave_id=$1
-slave_name=$2
-server_url=$3
-port=$4
-login=$5
-private_key=$6
-client_version=$7
-branch=$8
-
-
-
-\rm -rf tig_pool_$branch
-\mkdir tig_pool_$brand
-cd tig_pool_$brand
-
+# Kill any existing screens named pool_tig
 screen -ls | grep pool_tig | awk '{print $1}' | xargs -I {} screen -S {} -X kill
 
-wget https://raw.githubusercontent.com/tig-pool-nk/client/refs/heads/$branch/scripts/tig_pool_master.sh
+# Download and run the updated script
+wget "https://raw.githubusercontent.com/tig-pool-nk/client/refs/heads/$branch/scripts/tig_pool_master.sh"
 sudo chmod +x tig_pool_master.sh
-./tig_pool_master.sh -id_slave $slave_id -nom_slave $slave_name -ip $server_url -port $port -login $login -tok $private_key -url $server_url -v $client_version
+
+./tig_pool_master.sh \
+    -id_slave "$slave_id" \
+    -nom_slave "$slave_name" \
+    -ip "$server_url" \
+    -port "$port" \
+    -login "$login" \
+    -tok "$private_key" \
+    -url "$server_url" \
+    -v "$client_version" \
+    -b "$branch"
