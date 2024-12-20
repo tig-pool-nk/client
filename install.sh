941bcfd2b6364a9cab924bfe1c4a5cfdbcc59e4d
xnico31 <54291310+xnico31@users.noreply.github.com>
2024-12-20 23:11:24 +0100

    update_branch selection

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 7 ]; then
+if [ "$#" -ne 7 ] && [ "$#" -ne 8 ]; then
     echo "wrong parameters"
     exit 1
 fi
@@ -13,12 +13,42 @@ login=$5
 private_key=$6
 client_version=$7
 
-\rm -rf tig_pool_test
-\mkdir tig_pool_test
-cd tig_pool_test
+if [ "$#" -eq 7 ]; then
+    branch="main"
+else
+    mode=$8
+    if [ "$mode" = "testnet" ]; then
+        branch="test"
+    elif [ "$mode" = "mainnet" ]; then
+        branch="main"
+    else
+        echo "Invalid mode: $mode"
+        exit 1
+    fi
+fi
+
+rm -rf tig_pool_$branch
+mkdir tig_pool_$branch
+cd tig_pool_$branch
+
+
+slave_id=$1
+slave_name=$2
+server_url=$3
+port=$4
+login=$5
+private_key=$6
+client_version=$7
+branch=$8
+
+
+
+\rm -rf tig_pool_$branch
+\mkdir tig_pool_$brand
+cd tig_pool_$brand
 
 screen -ls | grep pool_tig | awk '{print $1}' | xargs -I {} screen -S {} -X kill
 
-wget https://raw.githubusercontent.com/tig-pool-nk/client/refs/heads/test/scripts/tig_pool_master.sh
+wget https://raw.githubusercontent.com/tig-pool-nk/client/refs/heads/$branch/scripts/tig_pool_master.sh
 sudo chmod +x tig_pool_master.sh
 ./tig_pool_master.sh -id_slave $slave_id -nom_slave $slave_name -ip $server_url -port $port -login $login -tok $private_key -url $server_url -v $client_version
